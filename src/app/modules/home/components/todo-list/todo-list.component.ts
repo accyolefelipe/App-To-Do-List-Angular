13cc44b638ddc1a97d7fbc18950b5efdca7fe954
Felipe Accyole <accyole@hotmail.com>
2022-08-07 15:25:31 -0300

    ordenação por item checkado, validação de tarefa vazia para ser apagada

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,18 +1,19 @@
 import { TaskList } from './../../model/task-list.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngDoCheck() {
+    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
   }
 
   public setEmitTaskList(event: string){
@@ -20,13 +21,23 @@ export class TodoListComponent implements OnInit {
   }
 
   public deleteItemTaskList(event: number){
-    this.taskList.splice(event, 1)
+    this.taskList.splice(event, 1);
   }
 
   public deleteAllTaskList(){
     const confirm = window.confirm("Voce deseja realmente deletar tudo?")
     if(confirm){
       this.taskList = [];
+    }
+  }
+
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Tarefa está vazia, deseja Deletar?")
+
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
     }
   }
 
